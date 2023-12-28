8614b75a1bc7fb6416d15fe02426a281f8acb3f0
mohammed-abozamel112 <mohammed.abozamel112@gmail.com>
2023-12-28 23:46:34 +0200

    hh

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,10 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class ProductSeeder extends Seeder
